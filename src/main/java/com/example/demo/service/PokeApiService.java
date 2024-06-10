a0792d664e71c591ffb08269370d511ea488fbeb
Luisleandroribeiro <alemaole.luis@gmail.com>
2024-06-09 22:15:08 -0300

    dio desafio desing patterns

---
+++ src/main/java/com/example/demo/service/PokeApiService.java
@@ -0,0 +1,17 @@
+package spring.designpatterns.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "pokeapi", url = "https://pokeapi.co/api/v2")
+public interface PokeApiService {
+
+    @GetMapping("/pokemon/{id}")
+    Object getPokemonBase(@PathVariable("id") String pokemonIdentifier);
+
+    @GetMapping("pokemon-species/{species}")
+    Object getPokemonSpecies(@PathVariable("species") String pokemonSpecies);
+
+    @GetMapping("evolution-chain/{id}")
+    Object getEvolutionChain(@PathVariable("id") String evolutionChainIdentifier);
+}
